51d689c72683255ccf3d6c999303f4927f64219f
surajadkhari <surajadkhari@gmail.com>
2021-10-19 15:43:43 +0545

    Moving to 5 Scrollable Widgets

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_recipes/circle_image.dart';
 import 'package:flutter_recipes/foodrlichtheme.dart';
 
-class AuthorCard extends StatelessWidget {
+class AuthorCard extends StatefulWidget {
   final String authorName;
   final String title;
   final ImageProvider imageProvider;
@@ -15,6 +15,12 @@ class AuthorCard extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<AuthorCard> createState() => _AuthorCardState();
+}
+
+class _AuthorCardState extends State<AuthorCard> {
+  bool _isFavorited = false;
+  @override
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.all(16),
@@ -24,7 +30,7 @@ class AuthorCard extends StatelessWidget {
             Row(
               children: [
                 CircleImage(
-                  imageProvider: imageProvider,
+                  imageProvider: widget.imageProvider,
                   imageRadius: 28,
                 ),
               ],
@@ -36,22 +42,26 @@ class AuthorCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  authorName,
+                  widget.authorName,
                   style: FooderlichTheme.lightTextTheme.headline2,
                 ),
                 Text(
-                  title,
+                  widget.title,
                   style: FooderlichTheme.lightTextTheme.headline3,
                 ),
               ],
             ),
             IconButton(
-                icon: const Icon(Icons.favorite_border),
+                icon:
+                    Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
                 iconSize: 30,
-                color: Colors.grey[400],
+                color: Colors.red[400],
                 onPressed: () {
-                  const snackBar = SnackBar(content: Text('Favorite Pressed'));
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  // const snackBar = SnackBar(content: Text('Favorite Pressed'));
+                  // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  setState(() {
+                    _isFavorited = !_isFavorited;
+                  });
                 }),
           ],
         ));
